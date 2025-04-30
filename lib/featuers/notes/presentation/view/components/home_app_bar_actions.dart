98dec129aac16b9859cdec443a497bec3189ff48
solimanesam <solimanesam17@gmail.com>
2025-04-30 19:02:46 +0300

    soliman

--- lib/featuers/notes/presentation/view/components/home_app_bar_actions.dart
+++ lib/featuers/notes/presentation/view/components/home_app_bar_actions.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:notes_app/core/constants/routes_constants.dart';
 import 'package:notes_app/core/constants/view_constants.dart';
 import 'package:notes_app/core/theme/dark_theme_data.dart';
@@ -8,43 +7,53 @@ import 'package:notes_app/core/theme/light_theme_data.dart';
 import 'package:notes_app/core/theme/text_styles.dart';
 
 List<Widget> appBarActions(BuildContext context) {
-    final IconData themeIcon =
-        Get.isDarkMode ? Icons.light_mode : Icons.dark_mode;
-    final List<String> languagesStrings = <String>[
-      "DeviceLanguage".tr,
-      ...ViewConstants.constantsLanguagesStrings
-    ];
-    return [
-      IconButton(
-          onPressed: () {
-            Get.toNamed(RoutesConstants.featuredNotesPageRouteName);
-          },
-          icon: Icon(Icons.star)),
-      IconButton(onPressed: () {
-        Get.toNamed(RoutesConstants.searchScreenPageRouteName);
-      }, icon: Icon(Icons.search)),
-      IconButton(
-          onPressed: () {
-            if (Get.isDarkMode) {
-              Get.changeTheme(getlighttheme);
-            } else {
-              Get.changeTheme(getdarktheme);
-            }
-          },
-          icon: Icon(themeIcon)),
-      IconButton(
-          onPressed: () {
-            Get.bottomSheet(Column(
-              children: List.generate(
-                languagesStrings.length,
-                (index) => TextButton(
-                    onPressed: () {},
-                    child: Text(languagesStrings[index],
-                        style: TextStyles.bold23(context: context)
-                            .copyWith(color:Colors.white))),
-              ),
-            ));
-          },
-          icon: Icon(Icons.language))
-    ];
-  }
+  String language;
+  final IconData themeIcon =
+      Get.isDarkMode ? Icons.light_mode : Icons.dark_mode;
+  final List<String> languagesStrings = <String>[
+    "DeviceLanguage".tr,
+    ...ViewConstants.constantsLanguagesStrings
+  ];
+  return [
+    IconButton(
+        onPressed: () {
+          Get.toNamed(RoutesConstants.featuredNotesPageRouteName);
+        },
+        icon: Icon(Icons.star)),
+    IconButton(
+        onPressed: () {
+          Get.toNamed(RoutesConstants.searchScreenPageRouteName);
+        },
+        icon: Icon(Icons.search)),
+    IconButton(
+        onPressed: () {
+          if (Get.isDarkMode) {
+            Get.changeTheme(getlighttheme);
+          } else {
+            Get.changeTheme(getdarktheme);
+          }
+        },
+        icon: Icon(themeIcon)),
+    IconButton(
+        onPressed: () {
+          Get.bottomSheet(Column(
+            children: List.generate(
+              languagesStrings.length,
+              (index) => TextButton(
+                  onPressed: () {
+                    language = languagesStrings[index];
+                    if(language == 'english'){
+                      Get.updateLocale(Locale('en'));
+                    }else if(language == 'العربيه'){
+                      Get.updateLocale(Locale('ar'));
+                    }
+                  },
+                  child: Text(languagesStrings[index],
+                      style: TextStyles.bold23(context: context)
+                          .copyWith(color: Colors.white))),
+            ),
+          ));
+        },
+        icon: Icon(Icons.language))
+  ];
+}
